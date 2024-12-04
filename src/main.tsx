64ded59b0360031640f6913441ffdd7817656c89
Matheus Eckel Kleinschmidt <mkleinschmidt@softexpert.com>
2024-12-04 16:10:27 -0300

    add map

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,9 @@ import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { NotFoundError, fetchPost, fetchPosts } from './posts'
 import type { ErrorComponentProps } from '@tanstack/react-router'
 import HomePage from "./pages/HomePage"
+// ⚠️ No types available here
+import { MapContainer } from 'react-leaflet'
+import { TileLayer } from "react-leaflet/TileLayer";
 
 const rootRoute = createRootRoute({
   component: RootComponent,
@@ -77,7 +80,12 @@ const indexRoute = createRoute({
 
 function IndexComponent() {
   return (
-    <HomePage></HomePage>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  </MapContainer>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
+// ⚠️ No types available here
+import { MapContainer } from 'react-leaflet'
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker } from "react-leaflet/Marker";
+import { Popup } from "react-leaflet/Popup";
+
 export default function HomePage() {
-    return (
-      <div className="p-2">
-        <h3 className="p-10">Welcome Home!</h3>
-      </div>
-    )
-  }
+  return (
+
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+
+  );
+}
